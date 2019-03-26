1000ca5d9411c4cf90dbbf4b9f1c4b02660badea
mahathirm <32903354+mahathirm@users.noreply.github.com>
2019-03-26 18:21:43 +0700

    Add files via upload

--- Tugas2/server.py
+++ Tugas2/server.py
@@ -2,20 +2,27 @@ import socket
 import time
 import sys
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005 
-filenya=["koin1.jpg", "koin2.jpg", "koin3.jpg", "koin4.jpg"]; 
 
-for data in filenya: 
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-	sock.sendto(data, (UDP_IP, UDP_PORT)) 
+UDP_IP=["127.0.0.1","127.0.0.2","127.0.0.3","127.0.0.4",];
+UDP_PORT = 9000
+buf = 1024
+file_name=["koin1.jpg","koin2.jpg","koin3.jpg","koin4.jpg"];
 
-	fp = open(data, "rb")
-	data = fp.read()
-	while(data):
-	    if(sock.sendto(data, (UDP_IP, UDP_PORT))):
-	        data = fp.read() 
+for UDEP in UDP_IP: 
+	for x in file_name: 
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-	sock.close()
-	fp.close() 
-	time.sleep(7)
+		sock.sendto(x, (UDEP, UDP_PORT))
+		print "Sending %s ..." % x
+
+		f = open(x, "rb")
+		data = f.read()
+		while(data):
+		    if(sock.sendto(data, (UDEP, UDP_PORT))):
+		        data = f.read()
+		        
+
+		sock.close()
+		f.close()
+		time.sleep(12)
